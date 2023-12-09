a0c0f854db272c0be51d6f827babbe2e5eade022
Pandu KW <pandu.usahaku@gmail.com>
2023-12-09 10:15:30 +0700

    refactor

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,23 +1,18 @@
 import 'package:authentifire/set_user_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 
 class SignUp {
-  final MockFirebaseAuth _firebaseAuth; // Only used for testing needs
-
-  // final firebase_auth.FirebaseAuth _firebaseAuth;
-  // final SetUserData _setUserData;
+  final firebase_auth.FirebaseAuth _firebaseAuth;
+  final SetUserData _setUserData;
 
   SignUp({
-    MockFirebaseAuth? firebaseAuth, // Only used for testing needs
-    // firebase_auth.FirebaseAuth? firebaseAuth,
-    // SetUserData? setUserData,
-  }) : _firebaseAuth =
-            firebaseAuth ?? MockFirebaseAuth(); // Only used for testing needs
-  // _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
-  // _setUserData = setUserData ?? SetUserData(FirebaseFirestore.instance);
+    firebase_auth.FirebaseAuth? firebaseAuth,
+    SetUserData? setUserData,
+  }) : _firebaseAuth = firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+  _setUserData = setUserData ?? SetUserData(FirebaseFirestore.instance);
 
   Future<void> signUp({
     required String email,
@@ -30,15 +25,15 @@ class SignUp {
       await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
 
-      // final userId = _firebaseAuth.currentUser?.uid;
+      final userId = _firebaseAuth.currentUser?.uid;
 
-      // await _setUserData.setUserData(
-      //   userId: userId!,
-      //   email: email,
-      //   username: username,
-      //   phoneNumber: phoneNumber,
-      //   authMethodType: 'email',
-      // );
+      await _setUserData.setUserData(
+        userId: userId!,
+        email: email,
+        username: username,
+        phoneNumber: phoneNumber,
+        authMethodType: 'email',
+      );
     } catch (e) {
       print('Exception occurred during sign up: $e');
       throw 'Failed to sign up.';
--- test/sign_up_test.dart
+++ test/sign_up_test.dart
@@ -1,167 +1,42 @@
-// import 'package:authentifire/mock_user.dart';
-// import 'package:authentifire/mock_user_credential.dart';
-// import 'package:authentifire/set_user_data.dart';
-// import 'package:authentifire/sign_up.dart';
-// import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-
-// // Buat mock untuk FirebaseAuth
-// class MockFirebaseAuth extends Mock implements firebase_auth.FirebaseAuth {}
-
-// // Buat mock untuk SetUserData
-// class MockSetUserData extends Mock implements SetUserData {}
-
-// void main() {
-//   late SignUp sut;
-//   late MockFirebaseAuth mockFirebaseAuth;
-//   late MockSetUserData mockSetUserData;
-
-//   setUp(() {
-//     mockFirebaseAuth = MockFirebaseAuth();
-//     mockSetUserData = MockSetUserData();
-//     sut = SignUp(
-//       firebaseAuth: mockFirebaseAuth,
-//       setUserData: mockSetUserData,
-//     );
-//   });
-
-//   test('Successful sign up test', () async {
-//     const email = 'john@example.com';
-//     const password = '12345678';
-//     const validUserId = 'validUserId';
-
-//     final mockUser = MockUser();
-
-//     // Buat objek mock untuk UserCredential
-//     final mockUserCredential = MockUserCredential(false, mockUser: mockUser);
-
-//     // Konfigurasi respons ketika metode dipanggil
-//     when(mockFirebaseAuth.createUserWithEmailAndPassword(
-//       email: email,
-//       password: password,
-//     )).thenAnswer((_) async => mockUserCredential);
-
-//     // Memanggil metode yang ingin diuji
-//     await sut.signUp(
-//       email: email,
-//       password: password,
-//       username: 'John Doe',
-//       phoneNumber: '1234567890',
-//       authMethodType: 'email',
-//     );
-
-//     // Verifikasi pemanggilan metode dengan argumen yang benar
-//     verify(mockFirebaseAuth.createUserWithEmailAndPassword(
-//       email: email,
-//       password: password,
-//     ));
-
-//     // Verifikasi pemanggilan metode setUserData dengan argumen yang benar
-//     verify(mockSetUserData.setUserData(
-//       userId: validUserId,
-//       email: email,
-//       username: 'John Doe',
-//       phoneNumber: '1234567890',
-//       authMethodType: 'email',
-//     )).called(1); // Pastikan bahwa metode ini dipanggil tepat satu kali
-//   });
-
-//   // Tambahkan pengujian untuk kasus lain jika diperlukan
-// }
-// import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-// import 'package:flutter_test/flutter_test.dart';
-
-// void main() {
-//   group('Returns a mocked user user after sign up', () {
-//     test('with email and password', () async {
-//       final email = 'some@email.com';
-//       final password = 'some!password';
-//       final auth = MockFirebaseAuth();
-//       final result = await auth.createUserWithEmailAndPassword(
-//           email: email, password: password);
-//       final user = result.user!;
-//       expect(user.email, email);
-//       final providerData = user.providerData;
-//       expect(providerData.length, 1);
-//       expect(providerData.first.providerId, 'password');
-//       expect(providerData.first.email, 'some@email.com');
-//       expect(providerData.first.uid, user.uid);
-
-//       // expect(auth.authStateChanges(), emitsInOrder([null, isA<User>()]));
-//       // expect(auth.userChanges(), emitsInOrder([null, isA<User>()]));
-//       expect(user.isAnonymous, isFalse);
-//       expect(user.emailVerified, isTrue);
-//     });
-
-//     test('with email and password without email verification by default',
-//         () async {
-//       final email = 'some@email.com';
-//       final password = 'some!password';
-//       final auth = MockFirebaseAuth(verifyEmailAutomatically: false);
-//       final result = await auth.createUserWithEmailAndPassword(
-//           email: email, password: password);
-//       final user = result.user!;
-//       expect(user.email, email);
-//       final providerData = user.providerData;
-//       expect(providerData.length, 1);
-//       expect(providerData.first.providerId, 'password');
-//       expect(providerData.first.email, 'some@email.com');
-//       expect(providerData.first.uid, user.uid);
-
-//       // expect(auth.authStateChanges(), emitsInOrder([null, isA<User>()]));
-//       // expect(auth.userChanges(), emitsInOrder([null, isA<User>()]));
-//       expect(user.isAnonymous, isFalse);
-//       expect(user.emailVerified, isFalse);
-//     });
-//   });
-// }
-// import 'package:authentifire/set_user_data.dart';
+import 'package:authentifire/set_user_data.dart';
 import 'package:authentifire/sign_up.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
+import 'package:mockito/mockito.dart';
 
 // Import class SignUp dan SetUserData
 
-// class MockSetUserData extends Mock implements SetUserData {}
+class MockSetUserData extends Mock implements SetUserData {}
 
 void main() {
   group('SignUp', () {
     test('should sign up user', () async {
+      // Arrange
       final mockFirebaseAuth = MockFirebaseAuth();
-      // final mockSetUserData = MockSetUserData();
       const validUserId = 'user123';
-      final signUp = SignUp(firebaseAuth: mockFirebaseAuth);
-
       final email = 'john@example.com';
       final password = '12345678';
+      final sut = SignUp(firebaseAuth: mockFirebaseAuth);
 
-      print('Before signing up user');
+      // Act
+      await sut.signUp(email: email, password: password);
 
-      await signUp.signUp(email: email, password: password);
-
-      print('After signing up user');
-
-      // Verifikasi bahwa createUserWithEmailAndPassword dipanggil dengan argumen yang benar
-      // verify(mockFirebaseAuth.createUserWithEmailAndPassword(
-      //   email: email,
-      //   password: password,
-      // )).called(1);
-
+      // Assert
       expect(
           mockFirebaseAuth.createUserWithEmailAndPassword(
               email: 'john@example.com', password: '12345678'),
           completion(anything));
+    });
 
-      // Verifikasi bahwa setUserData dipanggil dengan argumen yang benar
-      // verify(mockSetUserData.setUserData(
-      //   userId: validUserId,
-      //   email: email,
-      //   username: 'John',
-      //   phoneNumber: '+123456789',
-      //   authMethodType: 'email',
-      // )).called(1);
+    test('should add user data', () async {
+      // Arrange
+      final mockSetUserData = MockSetUserData();
+      final userId = 'user123';
+      final email = 'john@example.com';
+      final username = 'John';
+      final phoneNumber = '+123456789';
+      final authMethodType = 'email';
+      final sut = SignUp(set)
     });
   });
 }
